d66d90d12fb32dc6c49a47b54b506fa061a0977e
ayakagi <ayakagileo@gmail.com>
2020-08-10 23:58:05 +0800

    final answer

--- script.js
+++ script.js
@@ -1,47 +1,38 @@
-// class Forex{
-//   constructor(){
-//     let rates, new_rates;
-//   }
-
-//   // check if rate is even number
-//   checkIfEven(rate){
-//     return rate % 2 == 0 ? true : false;
-//   }
-// }
-
-   // get data from sending http request
+// get data from sending http request
 function getRates(){
   return fetch("http://data.fixer.io/api/latest?access_key=fe3a9b3f9ac173a3e51d8ea22951cf95")
-      .then(response => {
+    .then(response => {
       if(!response.ok){throw new Error("RESPONSE ERROR");}
       else{return response.json();}
-      })
-      .then(data => {
-        var currency = Object.keys(data.rates)
-        var rate = Object.values(data.rates)
-        var sec_rate = rate.map(function(val){return val+10.0002;});
-        console.log(currency);
-        console.log(rate);
-        console.log(sec_rate);
-        var table = ""
-        for(i=0; i<currency.length; i++){
-          table += (`
-            <tr>
-              <td>${currency[i]}</td>
-              <td>${rate[i]}</td>
-              <td>${sec_rate[i]}</td>
-            </tr>`
-          );
-        }
-        console.log(table)
-        document
-          .querySelector("#forex")
-          .insertAdjacentHTML("beforeend", table)
-      })
-      .catch(error => console.error(error));
+    })
+    .then(data => {
+      // variable that will be used in table
+      var currency = Object.keys(data.rates)
+      var rate = Object.values(data.rates)
+      var sec_rate = rate.map(function(val){return val+10.0002;});
+      var table = ""
+      
+      for(i=0; i<currency.length; i++){
+        // check if currency is HKD or rate is even number
+        // change its border color to red
+        var color1 = color2 = color3 = "";
+        if(currency[i]=="HKD"){color1=' class="red"'};
+        if(rate%2==0){color2=' class="red"'};
+        if(sec_rate%2==0){color3=' class="red"'}
+        // initialize html code
+        table += (`
+          <tr>
+            <td${color1}>${currency[i]}</td>
+            <td${color2}>${rate[i]}</td>
+            <td${color3}>${sec_rate[i]}</td>
+          </tr>
+        `);
+      }
+      document
+        .querySelector("#forex")
+        .insertAdjacentHTML("beforeend", table)
+    })
+    .catch(error => console.error(error));
 }
-// var forex = new Forex();
-// forex.rates = getRates()
-// console.log(forex.rates)
   
 getRates()
